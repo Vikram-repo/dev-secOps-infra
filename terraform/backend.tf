562a2063dc4c301e70d52c6a5edcde913465051b
vikramazure <shivam.prajapati@userpcdocgmail.onmicrosoft.com>
2025-10-07 15:58:33 +0000

    Updated backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "1.13.3"
   backend "azurerm" {
     access_key           = "AeaVypmCTfyGqIABbdQJdLQ99mYoHOMoR1L+VJmZeJgQG5OO/WdmgAsvSUazWKZ03X15ILqit2RB+AStL5RPmw=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
     storage_account_name = "sandisk11"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
