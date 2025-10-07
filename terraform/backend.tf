9011cdd0315b7a360fec5884074c4dc377bdcf8f
vikramazure <shivam.prajapati@userpcdocgmail.onmicrosoft.com>
2025-10-07 15:12:03 +0000

    Updated backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    access_key           = "KyZfhDVlbETYorG/kmtwhAgHkErpusX+NfD1F27or08HYXB/Huzd0+5e/PECyr/vI11vtlEiVNm4+AStUjjxXA=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
-    storage_account_name = "azurepipeline123"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "azure"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    access_key           = "AeaVypmCTfyGqIABbdQJdLQ99mYoHOMoR1L+VJmZeJgQG5OO/WdmgAsvSUazWKZ03X15ILqit2RB+AStL5RPmw=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    storage_account_name = "sandisk11"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "friday11"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
