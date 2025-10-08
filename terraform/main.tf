753d7b5753ea54de77c9cdf90fd82c2d14e14228
vikramazure <shivam.prajapati@userpcdocgmail.onmicrosoft.com>
2025-10-08 14:17:35 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "example" {
     for_each = var.rg
-  name     = each.value.name_rg
-  location = each.value.location_rg
+  name     = each.value.name
+  location = each.value.location
 }
