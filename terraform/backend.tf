e024b0a8eccaa341a2357426cadfffa830b5992a
vikramazure <shivam.prajapati@userpcdocgmail.onmicrosoft.com>
2025-10-08 12:04:13 +0000

    Updated backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "1.13.3"
   backend "azurerm" {
-    access_key           = "AeaVypmCTfyGqIABbdQJdLQ99mYoHOMoR1L+VJmZeJgQG5OO/WdmgAsvSUazWKZ03X15ILqit2RB+AStL5RPmw=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    access_key           = "jwIVGIL7wDfI5t2zQ6JAz6wvJrRNvQdoxlCs+E0Y7bX4bAH8eYzLWFQRJEXiHYFq7kQ4H0ALyDyi+AStW+N2nw=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
     storage_account_name = "sandisk11"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "friday11"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
