0eec888673fcd3a1ca161a9ce00d90034ee9746f
vikramazure <shivam.prajapati@userpcdocgmail.onmicrosoft.com>
2025-10-13 15:38:02 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,13 @@ resource "azurerm_resource_group" "example" {
   name     = each.value.name
   location = each.value.location
 }
+
+resource "azurerm_storage_account" "badstorage" {
+  name                     = "storageaccountname12334455"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
+  enable_https_traffic_only = false
+}
